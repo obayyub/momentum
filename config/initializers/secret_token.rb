cacc2288f4f8a6f6f7103826e142d602e6981557
Omar Ayyub <obayyub@gmail.com>
2013-03-14 02:22:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momentum::Application.config.secret_token = '9159352f770333980eac7408f90aa471de69c374d0bb3495948f3526e082520008e129f22018ee6d74fdd1dc4157c7151a8d623ea32a083928b08bb5dc976181'
